026b826c69293cbf247090c2b32f325e29bc1d76
seaaralee <mutiaranimeilani2805@gmail.com>
2022-02-19 16:04:51 +0700

    modul4

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,17 +4,18 @@
     <div class="title-top mb-5">
         <h2 class="fw-bold"> Contact Us </h2>
     </div>
-    <form>
+    <form method="post" action="{{route('contact.store')}}">
+        {{csrf_field()}}
     <div class="form-floating mb-3">
-        <input type="text" class="form-control" placeholder="Name">
+        <input type="text" class="form-control" placeholder="Name" name="name">
         <label id="nameid" for="inputname" class="form-label">Name</label>
     </div>
     <div class="form-floating mb-3">
-        <input type="email" class="form-control" placeholder="Email">
+        <input type="email" class="form-control" placeholder="Email" name="email">
         <label id="emailid" for="inputemail" class="form-label">Email</label>
     </div>
     <div class="form-floating mb-3">
-        <textarea id="messageid" class="form-control" placeholder="Enter your message . ." type="inputmessage"> </textarea>
+        <textarea id="messageid" class="form-control" placeholder="Enter your message . ." type="inputmessage" name="message"> </textarea>
         <label class="message">Message</label>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
